7bf7339fa9b4135f6457cad1c177e79a278042a6
Himeka Tsuchida <yuria@sfc.wide.ad.jp>
2022-01-06 22:41:23 +0900

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,33 @@
+import * as functions from "firebase-functions";
+import fetch from "node-fetch";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  const apiRequest = () => fetch("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=%E7%A5%AD%20%E5%91%8A%E7%9F%A5&targets=title&_sort=-startTime&_offset=1&_context=apitest&_limit=4&fields=contentId,title,description&filters[genre][0]=%E6%96%99%E7%90%86")
+      .then((x) => x.json());
+  const main = async () => {
+    const result = await apiRequest();
+    try {
+      result.data.forEach( async (res) => await db
+          .collection("raw")
+          .doc(res.contentId)
+          .set(res));
+    } catch (err) {
+      functions.logger.error(err);
+    }
+    functions.logger.info(result);
+    response.send(result);
+  };
+  main();
+});
+
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
