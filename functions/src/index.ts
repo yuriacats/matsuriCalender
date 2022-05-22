e159bca224e8314fa2c174f0ba5b0c886c86632c
Himeka Tsuchida <yuria.soft0615@gmail.com>
2022-05-22 18:21:35 +0900

    型の情報を付加した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,16 @@ import fetch from "node-fetch";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 const db = admin.firestore();
+type Content ={
+ "data":[{
+   "contentId": string
+   "title": string
+   "description": string
+ }]
+}
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  const apiRequest = () => fetch("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=%E5%91%8A%E7%9F%A5&targets=title&_sort=-startTime&_offset=1&_context=apitest&_limit=4&fields=contentId,title,description&filters[genre][0]=%E6%96%99%E7%90%86")
+  const apiRequest = ():Promise<Content> => fetch("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=%E5%91%8A%E7%9F%A5&targets=title&_sort=-startTime&_offset=1&_context=apitest&_limit=4&fields=contentId,title,description&filters[genre][0]=%E6%96%99%E7%90%86")
       .then((x) => x.json());
   const main = async () => {
     const result = await apiRequest();
